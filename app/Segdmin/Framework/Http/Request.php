0cb6773b42fae20ae0e7d6285c0a03e67a419b8e
Damián Nohales <damiannohales@gmail.com>
2012-10-20 14:28:18 -0300

    Se crean métodos para obtener información de la URL

--- app/Segdmin/Framework/Http/Request.php
+++ app/Segdmin/Framework/Http/Request.php
@@ -11,18 +11,15 @@ use Segdmin\Framework\Util\ArrayCollection;
 class Request
 {
 	private $_query;
-	
 	private $_post;
-	
 	private $_attributes;
-	
 	private $_cookies;
-	
 	private $_files;
-	
 	private $_server;
-	
 	private $method;
+	private $baseUrl;
+	private $baseRoutePath;
+	private $pathName;
 	
 	public function __construct(array $query = array(), array $post = array(),
 								 array $attributes = array(), array $cookies = array(),
@@ -36,6 +33,9 @@ class Request
 		$this->_server = new ArrayCollection($server);
 		
 		$this->method = strtoupper($this->server()->get('REQUEST_METHOD', 'GET'));
+		$this->baseUrl = $this->prepareBaseUrl();
+		$this->baseRoutePath = $this->prepareBaseRoutePath();
+		$this->pathName = $this->preparePathName();
 	}
 	
 	/**
@@ -87,4 +87,75 @@ class Request
 	{
 		return $this->getMethod() == 'POST';
 	}
+	
+	private function prepareBaseUrl()
+	{
+		return dirname($this->server()->get('SCRIPT_NAME'));
+	}
+	
+	/**
+	 * Devuelve la URL base del sistema, esto es, la URL que apunta a la carpeta
+	 * web. Este método nunca devuelve el nombre del script ejecutado. Además
+	 * la URL es devuelta sin información del servidor y protocolo.
+	 * 
+	 * http://localhost/sitio/web/index.php/login -> /sitio/web
+	 * http://localhost/ -> /
+	 * 
+	 * @return string La URL base
+	 */
+	public function getBaseUrl()
+	{
+		return $this->baseUrl;
+	}
+	
+	private function prepareBaseRoutePath()
+	{
+		if( $this->server()->has('REDIRECT_URL') ){
+			//Es una URL sin index.php (con rewrite)
+			return $this->getBaseUrl();
+		} else {
+			//Es una URL con index.php (sin rewrite)
+			return ($this->getBaseUrl() == '/'? '':$this->getBaseUrl()).'/'.trim(basename($this->server()->get('SCRIPT_NAME')), '/');
+		}
+	}
+	
+	/**
+	 * Devuelve el path base para generar las rutas al sistema, se diferencia
+	 * con getBaseUrl en que este método puede devolver el nombre del controlador
+	 * frontal (index.php) si no está presente la reescritura de URL.
+	 * 
+	 * @return string La URL base para generar rutas.
+	 */
+	public function getBaseRoutePath()
+	{
+		return $this->baseRoutePath;
+	}
+
+	private function preparePathName()
+	{
+		$path = '';
+		
+		if($this->server()->has('PATH_INFO')){
+			$path = trim($this->server()->get('PATH_INFO'), '/');
+		} else if($this->server()->has('REDIRECT_URL')) {
+			$path = trim(substr_replace($this->server()->get('REDIRECT_URL'), '', 0, strlen($this->getBaseUrl())), '/');
+		}
+		
+		if(empty($path)){
+			return '/';
+		} else {
+			return '/'.$path;
+		}
+	}
+	
+	/**
+	 * El nombre del path.
+	 * 
+	 * @return string El nombre del path
+	 */
+	public function getPathName()
+	{
+		return $this->pathName;
+	}
+
 }
