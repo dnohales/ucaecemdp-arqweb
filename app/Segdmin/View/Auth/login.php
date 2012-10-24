93bd6d8f61926c63adace4a97fd33cff494d09fa
Damian Nohales <damiannohales@gmail.com>
2012-10-24 13:44:01 -0300

    Creado formulario de login

--- app/Segdmin/View/Auth/login.php
+++ app/Segdmin/View/Auth/login.php
@@ -1,9 +1,31 @@
-<?php $this->extend(':base') ?>
+<?php $this->extend('Base:nomenu'); ?>
 
 <?php $this->block('title'); ?>Iniciar Sesión - <?php echo $this->parentBlock(); $this->endBlock() ?>
 
-<?php $this->block('body') ?>
+<?php $this->block('css') ?>
+	<?php echo $this->parentBlock() ?>
+	<link rel="stylesheet" type="text/css" href="<?php echo $this->asset('css/login.css') ?>" />
+<?php $this->endBlock() ?>
+
+<?php $this->block('maincontent') ?>
 <div class="login-container">
-	<form action="<?php echo $this->url('login') ?>"></form>
+	<h1>Iniciar sesión en Segdmin</h1>
+	<form method="post" action="<?php echo $this->currentUri(true) ?>">
+		<input class="fullwidth" name="email" type="text" placeholder="Correo electrónico" />
+		<input class="fullwidth" name="password" type="password" placeholder="Contraseña" />
+		<div class="login-buttons">
+			<div>
+				<label class="checkbox">
+					<input type="checkbox" />
+					Mantener sesión abierta
+				</label>
+			</div>
+			<div>
+				<button class="btn btn-primary" type="submit">Iniciar Sesión</button>
+			</div>
+		</div>
+		
+		<div class="clearfix"></div>
+	</form>
 </div>
 <?php $this->endBlock() ?>
--- app/Segdmin/View/Base/nomenu.php
+++ app/Segdmin/View/Base/nomenu.php
@@ -2,17 +2,26 @@
 
 <?php $this->block('body') ?>
 <div id="container">
-	<div id="header">
-	</div>
-	<div id="maincontent">
-		<?php $this->block('maincontent') ?>
-		<?php $this->endblock() ?>
+	<div id="wrapper">
+		<div id="header">
+			<div class="container">
+				acá va todo
+			</div>
+		</div>
+		<div class="container">
+			<div id="maincontent">
+				<?php $this->block('maincontent') ?>
+				<?php $this->endBlock() ?>
+			</div>
+		</div>
 	</div>
 	<div id="footer">
-		<h1>Universidad CAECE, Arquitectura Web, Trabajo Final</h1>
-		<p>Nohales Damián</p>
-		<p>Penovi Mariano</p>
-		<p>Lippolis Emiliano</p>
+		<div class="container">
+			<div class="footer-note">
+				<p>Copyright &copy; 2012 por Damián Nohales, Emiliano Lippolis y Mariano Penovi</p>
+				<p>Este programa es Software Libre, puede redistribuirlo y/o modificarlo bajo los términos de la <a href="http://www.gnu.org/licenses/agpl-3.0-standalone.html">Licencia Pública General de Affero (AGPL)</a></p>
+			</div>
+		</div>
 	</div>
 </div>
-<?php $this->endblock() ?>
+<?php $this->endBlock() ?>
