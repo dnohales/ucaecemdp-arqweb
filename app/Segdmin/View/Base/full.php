289dfb61b7da4d7f093476af51883fe9d2109a68
Damián Nohales <damiannohales@gmail.com>
2012-10-27 20:43:29 -0300

    Modificado el menú principal

--- app/Segdmin/View/Base/full.php
+++ app/Segdmin/View/Base/full.php
@@ -9,26 +9,19 @@
 			</form>
 		</li>
 		<li><hr /></li>
-		<li><a href="#">Inicio</a></li>
-		<li><a href="#">Aseguradoras</a></li>
-		<li><a href="#">Clientes</a></li>
-		<li><a href="#">Administradores</a></li>
+		<li><a href="<?php echo $this->url('index') ?>">Inicio</a></li>
+		<li><a href="<?php echo $this->url('company_index') ?>">Compañías aseguradoras</a></li>
+		<li><a href="<?php echo $this->url('index') ?>">Coverturas</a></li>
+		<li><a href="<?php echo $this->url('index') ?>">Productores</a></li>
+		<li><a href="<?php echo $this->url('index') ?>">Clientes</a></li>
+		<li><a href="<?php echo $this->url('index') ?>">Usuarios</a></li>
 		<li><hr /></li>
-		<li><a href="#">Mi cuenta</a></li>
-		<li><a href="#">Cerrar sesión</a></li>
+		<li><a href="<?php echo $this->url('index') ?>">Mi cuenta</a></li>
+		<li><a href="<?php echo $this->url('logout') ?>">Cerrar sesión</a></li>
 	</ul>
 </div>
 <div id="content" class="well span7">
-	
-
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porta hendrerit orci, quis congue nisi tristique ac. Fusce elementum volutpat augue, ut viverra massa molestie sed. Curabitur eget neque nunc, id ornare eros. Quisque eu orci ut tortor sagittis lacinia. Aliquam erat volutpat. Duis sed pulvinar mi. Ut magna dolor, convallis a auctor sed, mollis in magna. Pellentesque nec augue at sapien vestibulum tempor sed sit amet urna. Sed eget arcu ut risus convallis vulputate et ut diam. Duis vel ligula mi, id porttitor quam. Vestibulum pellentesque porta neque, et tincidunt tellus ornare vel. Quisque eget lacus diam. Maecenas interdum consectetur justo nec imperdiet. Integer vehicula accumsan mi, feugiat interdum quam adipiscing at. Etiam rutrum sollicitudin metus, nec gravida magna aliquam ac.</p>
-
-<p>Pellentesque elit lacus, suscipit ut dignissim et, ultricies vel elit. Fusce consectetur faucibus libero, a consequat augue pretium vitae. Donec convallis ultrices sagittis. Aliquam augue lacus, vehicula id tristique vitae, euismod non dui. Maecenas eleifend metus eget velit imperdiet commodo tincidunt ligula faucibus. Morbi nulla turpis, adipiscing at imperdiet et, volutpat in diam. Nullam quis odio vitae magna laoreet semper. Fusce pulvinar diam in enim gravida id pharetra neque ultricies.</p>
-
-<p>Phasellus nec risus at erat lacinia posuere. Fusce leo odio, ullamcorper ac sagittis vel, commodo at lorem. Ut quis tortor congue lacus suscipit lacinia. Integer faucibus viverra porttitor. Fusce non dolor vitae lectus vestibulum imperdiet in a purus. Aliquam a lorem tincidunt felis dignissim vehicula eu ut diam. Nam eget velit eu nisi porta interdum. Duis diam justo, bibendum ac pharetra sed, fringilla nec enim. Donec massa ante, interdum non molestie ut, suscipit a diam. Nam suscipit, dolor vel dapibus tempus, nibh dolor rhoncus ipsum, vitae gravida odio dui sed lacus. Integer laoreet mauris a metus facilisis malesuada. Proin scelerisque pellentesque lectus, a pellentesque arcu vulputate id. Nunc tincidunt turpis suscipit mi condimentum quis iaculis lacus adipiscing. Morbi quam lectus, pulvinar iaculis pharetra viverra, malesuada sit amet ipsum. Integer eget eros lectus, quis tincidunt felis. Maecenas sit amet dolor urna.</p>
-
-<p>Sed feugiat accumsan orci, suscipit venenatis ligula semper in. Maecenas accumsan mattis justo ac laoreet. Phasellus lacinia nunc lectus, et posuere metus. Fusce sollicitudin sem id massa aliquet vestibulum. Maecenas ut lectus augue. Pellentesque euismod sodales ante vel lobortis. Quisque convallis leo quis lacus auctor semper. Fusce mauris nunc, semper in tincidunt vitae, ultrices at nibh.</p>
-
-<p>Praesent faucibus, libero et semper condimentum, metus velit vulputate massa, at porttitor massa diam condimentum orci. Morbi ut libero lectus. Quisque gravida aliquet gravida. Vestibulum a nulla eget felis dignissim fermentum. Cras ullamcorper, magna in rhoncus mattis, nunc quam egestas libero, varius facilisis lorem elit ut dui. Nulla erat orci, hendrerit at eleifend nec, ultricies vitae quam. Mauris mauris ipsum, pellentesque at sollicitudin at, consequat sit amet odio. Sed gravida tristique facilisis. </p>
+	<?php $this->block('content') ?>
+	<?php $this->endBlock() ?>
 </div>
 <?php $this->endBlock() ?>
