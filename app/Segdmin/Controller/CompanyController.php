a9f876e034a4c165e0b57bafa4618358f04f0654
Damián Nohales <damiannohales@gmail.com>
2012-10-27 20:43:56 -0300

    Implementada alta de compañías

--- app/Segdmin/Controller/CompanyController.php
+++ app/Segdmin/Controller/CompanyController.php
@@ -2,11 +2,29 @@
 namespace Segdmin\Controller;
 
 use Segdmin\Framework\Controller;
+use Segdmin\Model\Company;
 
 class CompanyController extends Controller
 {
+	public function indexAction()
+	{
+		return $this->render('Company:index', array(
+			'companies' => $this->getOrm()->getRepository('Company')->findAll()
+		));
+	}
+	
 	public function addAction()
 	{
+		$company = new Company($this->getOrm());
+		
+		if($this->getRequest()->isPost()){
+			foreach($this->getRequest()->post() as $key => $value){
+				$company->{"set$key"}(trim($value));
+			}
+			$this->getOrm()->insert($company);
+			return $this->redirectByRoute('company_index');
+		}
+		
 		return $this->render('Company:add');
 	}
     
---
+++ app/Segdmin/View/Company/index.php
@@ -0,0 +1,40 @@
+<?php $this->extend('Base:full') ?>
+
+<?php $this->block('content') ?>
+<h1>Compañías aseguradoras</h1>
+
+<div class="entity-table-container">
+	<header>
+		<div>
+			<form onsubmit="return false;">
+				<div class="input-prepend">
+					<span class="add-on"><i class="icon-filter"></i></span>
+					<input type="text" placeholder="Filtrar" />
+				</div>
+			</form>
+		</div>
+		<div>
+			<a href="<?= $this->url('company_add') ?>" class="btn btn-success" type="submit"><i class="icon-plus-sign icon-white"></i> Añadir compañía</a>
+		</div>
+	</header>
+	<div class="clearfix"></div>
+	<table class="table">
+		<tr>
+			<th>#</th>
+			<th>Razón social</th>
+			<th>Dirección</th>
+		</tr>
+		<?php if($companies->isEmpty()): ?>
+			<tr><td colspan="3">No hay ninguna compañía en el sistema</td></tr>
+		<?php else: ?>
+			<?php foreach($companies as $c): ?>
+			<tr>
+				<td><?= $c->getId() ?></td>
+				<td><?= $c->getName() ?></td>
+				<td><?= $c->getAddress() ?></td>
+			</tr>
+			<?php endforeach; ?>
+		<?php endif; ?>
+	</table>
+</div>
+<?php $this->endBlock() ?>
