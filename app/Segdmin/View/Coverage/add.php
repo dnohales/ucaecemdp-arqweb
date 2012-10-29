fb186affb2a5a9f1a838b7c8e044e7872c2ab74e
Damián Nohales <damiannohales@gmail.com>
2012-10-29 13:20:42 -0300

    Pequeña vista para añadir coberturas

--- app/Segdmin/View/Coverage/add.php
+++ app/Segdmin/View/Coverage/add.php
@@ -1,24 +1,24 @@
 <?php $this->extend('Base:full') ?>
 
-<?php $this->block('title'); ?>
-Agregar nueva cobertura - 
-<?php echo $this->parentBlock();
-$this->endBlock()
-?>
+<?php $this->block('title'); ?>Agregar nueva cobertura - <?php echo $this->parentBlock(); $this->endBlock() ?>
 
 <?php $this->block('content') ?>
-<form action="<?php echo $this->url('addcoverage') ?>" method="post">
-    <p>
-        <label for="description">Descripción:</label>
-    </p>
-    <textarea name="description" rows="5" cols="50"></textarea>
-    <p>
-        <label for="rate">Tasa:</label>
-        <input type="text" name="rate"/> %
-    </p>
-    <div class="submit">
-        <input type="reset" value="Reiniciar campos">
-        <input type="submit" value="Agregar cobertura">
-    </div>
+<h1>Agregar nueva cobertura</h1>
+
+<form action="<?= $this->currentUri() ?>" method="post">
+	<label>Compañía</label>
+	<select name="companyId" required>
+		<option value="">Seleccione...</option>
+		<?php foreach($companies as $c): ?>
+			<option value="<?= $c->getId() ?>"><?= $c->getId().': '.$c->getName() ?></option>
+		<?php endforeach; ?>
+	</select>
+	<label>Descripcion</label>
+	<input type="text" name="description" required />
+	<label>Porcentaje de la tasa</label>
+	<input type="text" name="rate" required />
+	<div class="button-list">
+		<button type="submit" class="btn btn-success">Crear cobertura</button>
+	</div>
 </form>
 <?php $this->endBlock() ?>
